dbe1c819a022d190d3be1996cdec258600f0a892
Lydia Tan <lydia.sydia@gmail.com>
2018-07-12 18:14:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 //Code your solution here
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
+
+function nestedTarget(){
+  return document.querySelector("#nested .target")
+}
+
+function deepestChild(){
+  return document.querySelector("#grand-node div div div div")
+}
+
+function increaseRankBy(n){
+  const rankedLists = document.querySelectorAll('.ranked-list')
+  
+  for(let i = 0; i < rankedLists.length; i++){
+    let children = rankedLists[i].children
+  
+  for(let j = 0, k = children.length; j < k; j++){
+    children[j].innerHTML = parseInt(children[j].innerHTML) + n
+  }
+}
+}
